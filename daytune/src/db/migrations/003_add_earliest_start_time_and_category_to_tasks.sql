ccae1a3290a5f770a06b1faa879f2c635cc50a46
RishiMac <87948611+RishiMac@users.noreply.github.com>
2025-05-15 16:23:02 -0400

    pre Mobiscroll removal

--- daytune/src/db/migrations/003_add_earliest_start_time_and_category_to_tasks.sql
+++
@@ -1,4 +0,0 @@
--- Add earliest_start_time and category columns to tasks
-ALTER TABLE tasks
-ADD COLUMN earliest_start_time TIME,
-ADD COLUMN category TEXT; 
--- daytune/src/db/migrations/004_add_earliest_start_date_to_tasks.sql
+++
@@ -1,3 +0,0 @@
--- Add earliest_start_date column to tasks
-ALTER TABLE tasks
-ADD COLUMN earliest_start_date DATE; 
