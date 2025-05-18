b76be9257c343e068fd5e71005adc79117df18e1
RishiMac <87948611+RishiMac@users.noreply.github.com>
2025-05-17 21:48:08 -0400

    Add react-icons dependency

---
+++ build/static/js/198.1dca982a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdaytune=self.webpackChunkdaytune||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.1dca982a.chunk.js.map
